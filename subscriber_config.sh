29955a5499a0d1d2d13678d6705ac2e99db0e465
George Jones <gjones@db1.corvexconnected.com>
2019-05-24 18:31:47 -0500

    added additional lines to kick off the db creation and change the localconfig.ini. Need to capture the output from the addSubscriber.cgi and extract the SID.

--- subscriber_config.sh
+++ subscriber_config.sh
@@ -1,17 +1,19 @@
 # Subscriber setup automation
-# by George Jones
+# by George Jones 
 # I'll put more stuff in here as I go along.
 
 #!/bin/bash
 
 # grab the subscriber name and set up the scripts directory
 
-subscriber=$1
+realname=$1
+subscriber=$2
 scriptscustomer="/var/www/db1/scripts-"$subscriber
+subpath="scripts-"$subscriber
 
 #confirm the subscriber name and exit if no or invalid input
 read -r -p "Configuring client name $subscriber . Is this correct? [y/n] " input
-
+ 
 case $input in
     [yY][eE][sS]|[yY])
  echo "Yes"
@@ -27,12 +29,21 @@ case $input in
 esac
 
 # copy the data from the MASTER directory to the subscriber dir
-echo "yes selected"
+
 echo "creating "$scriptscustomer
-
 cp -a /var/www/db1/MASTER $scriptscustomer
-
 echo $scriptscustomer " created"
 
-#run the perl script and capture the generated subscriber ID
+# to-do: run the cgi script and capture the script's output
+# parse that output and grab the subscriber ID.
+# Stick the subscriber ID into varibale "sid"
+
+echo "Creating database for "$realname
+perl /home/dib/scripts-testing/addSubscriber.cgi name=$realname prefix=$subscriber
+
+# change the value of the subscriberID and servicepath in localconfig.ini
+
+sed -i 's/scripts-CUSTOMER/'$subpath'/g' localconfig.ini
+sed -i 's/NNN/'$sid'/g' localconfig.ini
+
 
