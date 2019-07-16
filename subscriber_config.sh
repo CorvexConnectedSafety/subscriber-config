39364ee2c926366cf3c6df298cb05ea9bde02b1a
George Jones <gjones@db1.corvexconnected.com>
2019-07-16 14:31:25 -0500

    Added missing lsyncd config and changed the paths in the scrpit to complete paths so the script can be executed from anywhere. Cleaned up the comments and added a script usage note.

--- subscriber_config.sh
+++ subscriber_config.sh
@@ -1,6 +1,7 @@
 # Subscriber setup automation
-# by George Jones 
-# I'll put more stuff in here as I go along.
+# by George Jones for Corvex Connected Safety
+# 
+# usage: ./subscriber-config <company full name> <prefix/subscriber name>
 
 #!/bin/bash
 
@@ -34,10 +35,10 @@ echo "creating "$scriptscustomer
 cp -a /var/www/db1/MASTER $scriptscustomer
 echo $scriptscustomer " created"
 
-# Note to anyone using this script: Change the path/to/addSubscriber.cgi in order for this to work properly. 
+# Note to anyone using this script: Change the path/to/addSubscriber.cgi in order to use this for testing purposes with non-production files
 
 echo "Creating database for "$realname
-perl /home/gjones/scripts-testing/addSubscriber.cgi name=$realname prefix=$subscriber | | sed 1d | jq -r '.subscriber[].subscriberID' > /tmp/sid.txt
+perl $scriptscustomer/addSubscriber.cgi name=$realname prefix=$subscriber | sed 1d | jq -r '.subscriber[].subscriberID' > /tmp/sid.txt
 read sid < /tmp/sid.txt
 
 # change the value of the subscriberID and servicepath in localconfig.ini
@@ -48,3 +49,17 @@ sed -i 's/NNN/'$sid'/g' localconfig.ini
 # clean up the file from the temp directory
 
 rm /tmp/sid.txt
+
+# Add the backend to the syncronization tool
+echo "Adding backend to the sync tool"
+
+fullsid="000"$sid
+
+#create the lsync config file in the customer name
+cp /var/www/lsync.conf.d/MASTER /var/www/lsync.conf.d/$subscriber
+
+#edit the newly created file and update the subscriberID and customer name
+sed -i 's/CUSTOMER/'$subscriber'/g' /var/www/lsync.conf.d/$subscriber
+sed -i 's/NNNNNN/'$fullsid'/g' /var/www/lsync.conf.d/$subscriber
+
+echo "Backends setup for "$realname" completed. Please restart lsyncd."
