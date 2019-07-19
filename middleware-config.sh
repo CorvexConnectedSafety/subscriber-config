c500c189f7b356f4cb477caff79ccc0d4b500a44
mrjonestech <misterjonestech@gmail.com>
2019-07-19 20:08:49 +0000

    removed the customer variable as it's redundant, changed all instances to subscriber, renamed subscribername variable to just subscriber, replaced instances of the url as a path to be represented by  instead of .corvexconnected.com, updated the comments.

--- middleware-config.sh
+++ middleware-config.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 #Middleware config script
+# usage: ./middleware-config.sh <subscriber name> <sid>
+# 
+# Note: sid = Subscriber ID from subscriber-config.sh with the leading 0 removed
+# to-do, take into account for 3-digit subscriber id's 
+# These will be collected from the subscriber-config.sh script that was just run on db1
 
-customer=$1
-subscribername=$2
-sid=$3
-url=$customer".corvexconnected.com"
+subscriber=$1
+sid=$2
+url=$subscriber".corvexconnected.com"
 
 #set up the web app
 
-cp -a /var/www/MASTER /var/www/$customer.corvexconnected.com
+cp -a /var/www/MASTER /var/www/$url
 
-#change the .htaccess file in /var/var/www/$customer.corvexconnected.com/html/.htaccess
+#change the .htaccess file in /var/var/www/CUSTOMER.corvexconnected.com/html/.htaccess
 
-sed -i 's/SUBSCRIBERID/'$sid'/g' /var/www/$customer.corvexconnected.com/html/.htaccess
-sed -i 's/CUSTOMER/'$customer'/g' /var/www/$customer.corvexconnected.com/html/.htaccess
-sed -i 's/SUBSCRIBER NAME/'$subscribername'/g' /var/www/$customer.corvexconnected.com/html/.htaccess
+sed -i 's/SUBSCRIBERID/'$sid'/g' /var/www/$url/html/.htaccess
+sed -i 's/CUSTOMER/'$subscriber'/g' /var/www/$url/html/.htaccess
+sed -i 's/SUBSCRIBER NAME/'$subscriber'/g' /var/www/$url/html/.htaccess
 
 # set up Apache
 echo "Setting up Apache for the new customer"
-cp -a /etc/apache2/sites-available/MASTER /etc/apache2/sites-available/$customer.corvexconnected.com
+cp -a /etc/apache2/sites-available/MASTER /etc/apache2/sites-available/$url
 
 #edit the config file and change the CUSTOMER name to the name entered on the command line
 echo "editing the apache config file"
-sed -i 's/CUSTOMER/'$customer/'g' /etc/apache2/sites-available/$customer.corvexconnected.com
+sed -i 's/CUSTOMER/'$subscriber/'g' /etc/apache2/sites-available/$url
 
 # enable the site
 echo "Enabling the site"
